5a12edf9e5907b4993450671a82ace72bcc4f97f
pippotek <filippo.costamagna@hpc-head-n2.unitn.it>
2025-02-15 14:14:12 +0100

    pack benchmarks

--- code/parallel_prova.c
+++ code/parallel_prova.c
@@ -132,6 +132,9 @@
              if (from > max_node) max_node = from;
              if (to   > max_node) max_node = to;
              edge_count++;
+             if (edge_count >= 225000000){
+                break;
+             }
          }
      }
      fclose(file);
